5637c48bd1b4844fe92153e283824f0f926a2d49
Dneifiend <dneifiend@gmail.com>
2022-02-17 18:53:01 +0900

    fetch handler

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
 self.addEventListener('install', function(e) {
     console.log('[Service Worker] Install');
 });
+
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch event fired.', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            if (response) {
+                console.log('[ServiceWorker] Retrieving from cache...');
+                return response;
+            }
+            console.log('[ServiceWorker] Retrieving from URL...');
+            return fetch(e.request).catch(function (e) {
+               //you might want to do more error checking here too,
+               //eg, check what e is returning..
+               alert('You appear to be offline, please try again when back online');
+            });
+        })
+    );
+});

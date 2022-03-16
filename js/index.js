2874219f08a90b1706ee2a2827dcc91aaeaff154
Dneifiend <dneifiend@gmail.com>
2022-03-16 18:29:23 +0900

    snackbar / setting

---
+++ js/index.js
@@ -0,0 +1,40 @@
+let settingHandler = {
+    toggle : function (){
+        var visibility = document.querySelector('#modal').style.visibility ? 'hidden' : 'visible'
+        document.querySelector('#modal').style.visibility = visibility
+    },
+    close : function (ele, event){
+        if(event.target.id==='modal'){
+            document.querySelector('#modal').style.visibility = 'hidden'
+            return
+        }
+
+    },
+    on : function (event){
+        
+        // var userName = document.querySelector('#user-name-text-input').value
+        var userName = localStorage.userName || ''
+
+
+        var MDCTextField = mdc.textField.MDCTextField;
+        var textField = new MDCTextField(document.querySelector('.mdc-text-field'))
+        textField.value = localStorage.userName
+        document.querySelector('#modal').style.visibility = 'visible'
+    }, 
+    submit: function(charName){
+        var userName = document.querySelector('#user-name-text-input').value
+        localStorage.setItem('userName', userName)
+
+
+        var MDCSnackbar = mdc.snackbar.MDCSnackbar
+        var sn = new MDCSnackbar(document.querySelector('.mdc-snackbar'))
+        sn.labelText = `저장완료! 이제부터 우측 상단 캐릭터 아이콘을 클릭하여 '${userName}' 캐릭터만 필터가 가능합니다.`
+        sn.open()
+
+        settingHandler.toggle()
+    },
+
+    filterToggle: function(){
+        console.log('toggle userName Filter')
+    }
+}
--- render.js
+++ render.js
@@ -1,38 +1,66 @@
-var data = {
-    datasets: [{
-        
-        data: [12, 7],
-        backgroundColor: [
-            "#1EB980",
-            "#045d56"
-        ],
-        borderWidth: 0,
-        cutout: "85%",
-        tooltip:false
-    }]
+document.addEventListener('readystatechange', e => {
+    if(e.target.readyState === 'complete' ){
+        charInit()
+        mdcinit()
+    }
+})
 
-}
 
-var config = {
-    type: 'doughnut',
-    data: data,
-
-    options: {
-        
-
-        plugins:{
-            tooltip: {
-                enabled: false
-            },
-            title: {
-                display: false
+function charInit(){
+    var data = {
+        datasets: [{
+    
+            data: [12, 7],
+            backgroundColor: [
+                "#1EB980",
+                "#045d56"
+            ],
+            borderWidth: 0,
+            cutout: "85%",
+            tooltip: false
+        }]
+    
+    }
+    
+    var config = {
+        type: 'doughnut',
+        data: data,
+    
+        options: {
+    
+    
+            plugins: {
+                tooltip: {
+                    enabled: false
+                },
+                title: {
+                    display: false
+                }
             }
         }
-    }
+    
+    };
+    
+    var myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+}
 
-};
 
-var myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-);
+
+function mdcinit (){
+    var MDCTextField = mdc.textField.MDCTextField;
+    var MDCRipple = mdc.ripple.MDCRipple;
+    var MDCTextFieldIcon = mdc.textField.MDCTextFieldIcon;
+
+    
+    var textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+    textField.value = "ddddddd"
+    var icon = new MDCTextFieldIcon(document.querySelector('.mdc-text-field__icon'));
+    var mdcbtn = [].map.call(document.querySelectorAll('.mdc-button'), function (el) {
+        return new MDCRipple(el);
+    });
+}
+
+

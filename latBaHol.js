5524d53d319469b9f6fb31cdd83052c0535eec37
Dneifiend <dneifiend@gmail.com>
2022-03-21 15:44:10 +0900

    완료 얼럿 변경

--- latBaHol.js
+++ latBaHol.js
@@ -456,7 +456,6 @@ class Raid {
                     var _confirm = confirm(`정말 '${raidinfo.name}${raidinfo.time === "" ? "" : ` (${raidinfo.time})`}' 레이드를 완료하시겠습니까?`)
                     if (_confirm) {
                         // TODO post로 레이드 이름, 시간을 보내 시간을 완료로 변경
-                        alert('완료')
                         fetch('https://script.google.com/macros/s/AKfycbxz8bm2b9BrHUGi3GrgPMdF1kP6cXqjeofI2Q1MWQPNJ-5zs7phHS1c5IGsTFORBHJ6/exec',
                         {
                             method:'POST',
@@ -470,6 +469,7 @@ class Raid {
                             })
                         })
                         .then(res=>{
+                            alert('완료')
                             window.location.reload()
                         })
                         .catch(err=>{console.log(err)})
